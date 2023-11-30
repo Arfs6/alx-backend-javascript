ccc445a89e8806c0b9b5bd1b79018b161bc24ba5
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-11-30 05:57:23 +0100

    done with task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import {createUser, uploadPhoto} from 'utils.js';
+export default function handleProfileSignup() {
+  Promise.all(createUser(), uploadPhoto()).then((result) => {
+    console.log(`${result.body} ${result.firstName} ${result.lastName}`);
+  })
+    .catch(() => {
+      console.log('log Signup system offline');
+    });
+}
