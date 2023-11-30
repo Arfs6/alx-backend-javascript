91ffefd79bcd8a0620a46467e977b534eead07af
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-11-30 20:08:16 +0100

    done task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+/* eslint-disabled */
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const classRooms = [];
+  classRooms.push(new ClassRoom(19));
+  classRooms.push(new ClassRoom(20));
+  classRooms.push(new ClassRoom(34));
+  return classRooms;
+}
