790bf565c9a1483339df6df3fe15ddd7d9202a19
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-12-01 05:59:54 +0100

    done with task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+import Currency from '3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (Number.isNumber(amount)) {
+      this._amount = amount;
+    } else {
+      throw (new TypeError('Amount must be a number'));
+    }
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw (new TypeError('Currency must be a Currency'));
+    }
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (Number.isInteger(amount)) {
+      this._amount = amount;
+    } else {
+      throw (new TypeError('Amount must be a number'));
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      throw (new TypeError('Currency must be a Currency'));
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
